fa06407c7d69e3c78db5a106b14aeaa20f0e99b3
Marianzzz <marian0412510301@gmail.com>
2025-06-07 15:28:15 +0300

    project setup backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("signup roure");
+}
+export const login = (req, res) => {
+  res.send("login roure");
+}
+export const logout = (req, res) => {
+  res.send("logout roure");
+}
