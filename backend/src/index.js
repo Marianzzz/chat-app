319b812fdd4b09f9ceba0db728355222e0fb9750
Marianzzz <marian0412510301@gmail.com>
2025-06-11 18:38:55 +0300

    create generateToken JWT and create server function signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,15 @@
-import dotenv from 'dotenv';
-dotenv.config(); 
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
-import './lib/db.js'; 
+import "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
